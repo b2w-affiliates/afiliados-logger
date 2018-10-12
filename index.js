de8b9738e714d3fa26a8109e5bf3657ba4851e02
igorgcosta <igorgabriel@outlook.com>
2018-10-12 18:23:09 -0300

    removing unecessary libs and refactoring

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
-const logger = require('./src/logger');
+const winston = require(winston)
+const moment = require('moment-timezone')
 
-module.exports = {
-  logger
-};
+const LOG_LEVEL = {
+  error: 'error',
+  silly: 'silly',
+  debug: 'debug',
+  warn: 'warn',
+  warning: 'warn',
+  info: 'info',
+  verbose: 'verbose',
+}[process.env.LOG_LEVEL] || -1;
+
+// create formatter for dates used as timestamps
+const tsFormat = () => moment().tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS Z')
+
+const consoleOptions = {
+  level: LOG_LEVEL,
+  formatter({ level, message, meta }) {
+    return JSON.stringify({ at: tsFormat(), level, message, meta })
+  }
+}
+
+module.exports = new winston.Logger({
+  transports: [ new winston.transports.Console(consoleOptions) ],
+})
