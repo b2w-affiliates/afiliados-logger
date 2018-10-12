41a55ec47a0e193ba1d315b5365c5395fc2b7d3a
igorgcosta <igorgabriel@outlook.com>
2018-10-12 18:21:06 -0300

    Revert "removing unecessary libs and refactoring"
    This reverts commit ed8556d4f4faf1d37f2e9bc3891b0608605e3933.

--- index.js
+++ index.js
@@ -1,26 +1,5 @@
-const winston = require(winston)
-const moment = require('moment-timezone')
+const logger = require('./src/logger');
 
-const LOG_LEVEL = {
-  error: 'error',
-  silly: 'silly',
-  debug: 'debug',
-  warn: 'warn',
-  warning: 'warn',
-  info: 'info',
-  verbose: 'verbose',
-}[process.env.LOG_LEVEL] || -1;
-
-// create formatter for dates used as timestamps
-const tsFormat = () => moment().tz('America/Sao_Paulo').format('YYYY-MM-DD HH:mm:ss.SSS Z')
-
-const consoleOptions = {
-  level: LOG_LEVEL,
-  formatter({ level, message, meta }) {
-    return JSON.stringify({ at: tsFormat(), level, message, meta })
-  }
-}
-
-module.exports = new winston.Logger({
-  transports: [ new winston.transports.Console(consoleOptions) ],
-})
+module.exports = {
+  logger
+};
